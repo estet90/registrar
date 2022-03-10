2eb6b101ff5d6f2fd891a586314e29c8d6cf4217
dkononov <estetis21@gmail.com>
2022-03-10 17:24:44 +0400

    убрал jooq

--- src/main/java/ru/craftysoft/registrar/configuration/DbConfiguration.java
+++ src/main/java/ru/craftysoft/registrar/configuration/DbConfiguration.java
@@ -1,10 +1,6 @@
 package ru.craftysoft.registrar.configuration;
 
 import io.vertx.mutiny.pgclient.PgPool;
-import org.jooq.DSLContext;
-import org.jooq.SQLDialect;
-import org.jooq.conf.Settings;
-import org.jooq.impl.DefaultDSLContext;
 import ru.craftysoft.registrar.util.DbClient;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -15,11 +11,6 @@ public class DbConfiguration {
     @ApplicationScoped
     DbClient dbClient(PgPool pgPool) {
         return new DbClient(pgPool);
-    }
-
-    @ApplicationScoped
-    DSLContext dslContext() {
-        return new DefaultDSLContext(SQLDialect.POSTGRES, new Settings().withRenderFormatted(true));
     }
 
 }
