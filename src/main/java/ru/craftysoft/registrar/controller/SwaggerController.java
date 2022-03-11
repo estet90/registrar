051ac0e9863bf0bd8bf9898e1afda199860f73a5
dkononov <estetis21@gmail.com>
2022-03-11 14:08:16 +0400

    swagger controller fix

--- src/main/java/ru/craftysoft/registrar/controller/SwaggerController.java
+++ src/main/java/ru/craftysoft/registrar/controller/SwaggerController.java
@@ -5,24 +5,24 @@ import io.smallrye.mutiny.Uni;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-
 import java.io.IOException;
 
 import static java.util.Objects.requireNonNull;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
 @Path("/swagger")
-@Produces("text/yaml;charset=UTF-8")
+@Produces(TEXT_PLAIN)
 public class SwaggerController {
 
     private final byte[] swagger;
 
     public SwaggerController() throws IOException {
-        this.swagger = requireNonNull(getClass().getResourceAsStream("/openapi/registrar.yaml")).readAllBytes();;
+        this.swagger = requireNonNull(getClass().getResourceAsStream("/openapi/registrar.yaml")).readAllBytes();
     }
 
     @GET
     @Path("/registrar.yaml")
     public Uni<byte[]> companySwagger() {
-        return Uni.createFrom().item(swagger);
+        return Uni.createFrom().item(this.swagger);
     }
 }
